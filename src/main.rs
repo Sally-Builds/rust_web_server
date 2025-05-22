44f85e7549b7e94e4725a27b9c9b63731f2f673e
Joshua Uzoagulu <joshua.uzoagulu@netapps.com.ng>
2025-05-23 00:11:20 +0100

    A touch of refactoring

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
 
 }
 
+
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
 
@@ -20,34 +21,26 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let mut file = File::open("index.html").unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n", "index.html")
+    }else {
+        ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
         let mut html_contents = String::new();
         file.read_to_string(&mut html_contents).unwrap();
     
         // println!("{}", html_contents);
     
         let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}",
+            "{}Content-Type: text/html\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
             html_contents.len(),
             html_contents
         );
 
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
-    } else {
-        let mut file = File::open("404.html").unwrap();
-        let mut html_contents = String::new();
-        file.read_to_string(&mut html_contents).unwrap();
-
-        let response = format!(
-            "HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}",
-            html_contents.len(),
-            html_contents
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
 
 }
