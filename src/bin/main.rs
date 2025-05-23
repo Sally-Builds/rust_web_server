52ec29f4c9028c8f794efacd9e1725e503c1ad47
Joshua Uzoagulu <joshua.uzoagulu@netapps.com.ng>
2025-05-23 12:54:28 +0100

    creating workers and threads

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,18 @@
-use std::{fs::File, io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::{fs::File, io::{Read, Write}, net::{TcpListener, TcpStream}, time::Duration};
+use std::thread;
+use hello_server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+
     }
 
 
@@ -20,10 +27,15 @@ fn handle_connection(mut stream: TcpStream) {
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n", "index.html")
-    }else {
+    }else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n", "index.html")
+    }
+    else {
         ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
     };
 
